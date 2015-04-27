118cd7ce9bc145aded892e79c0507e532fa6a0ad
Weston Vial <wes@wesvial.com>
2015-04-27 08:00:53 -0400

    Form validation completed

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,9 +6,10 @@ from django.utils.html import escape
 
 from lists.models import Item, List
 from lists.views import home_page
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
-
-from unittest import skip
+from lists.forms import (
+        DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+        ExistingListItemForm, ItemForm,
+)
 
 class HomePageTest(TestCase):
     maxDiff = None
@@ -81,7 +82,7 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.post('/lists/%d/' % (list_.id,))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
     def post_invalid_input(self):
@@ -102,13 +103,12 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    @skip
     def test_duplicate_item_validation_errprs_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -117,8 +117,8 @@ class ListViewTest(TestCase):
                 data={'text': 'textey'}
         )
 
-        expected_error = escape("You've already got this in your list")
-        self.assertContains(response, espected_error)
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
+        self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
 
